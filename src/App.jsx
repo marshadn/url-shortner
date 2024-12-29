dfab6d6fe1f58e302a63fdfaf5edc78c51b3c47e
marshadn <muhamedmarshad18@gmail.com>
2024-12-29 10:34:01 +0530

    complete project

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
       {
         path: "/dashboard",
         element: (
-         // <RequireAuth>
+          //<RequireAuth>
             <Dashboard />
           //</RequireAuth>
         ),
